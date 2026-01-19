cb31d85e284e05c3ca0581999d87c833bb732c81
Aryan <aryanshah0514@gmail.com>
2026-01-20 00:43:45 +0100

    feat: set base site subfolder for the chessbot on frontend

--- chatbot/astro.config.mjs
+++ chatbot/astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.l145.be",
+  base: "/nnue-chessbot",
   integrations: [react()],
   output: "static",
   devToolbar: {
